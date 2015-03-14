99d401d4a217e5ebaafabd1f10903783f854d8f8
wade712vn <wade712vn@gmail.com>
2015-03-14 12:33:26 +0700

    Another commit

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,6 +3,10 @@ module ApplicationHelper
     content_for :page_title, page_title.to_s
   end
 
+  def body_id(body_id)
+    content_for :body_id, body_id.to_s
+  end
+
   def capitalize_first_word_letter(input)
     input.mb_chars.capitalize.to_s
   end
