fb68c32bd218c04a53139d74161318517e3b6f39
Michealz <duongtuananh4711@gmail.com>
2015-03-17 17:09:16 +0700

    a

--- app/assets/javascripts/pledges.js
+++ app/assets/javascripts/pledges.js
@@ -1,5 +1,20 @@
 (function ($) {
 
-
+	$('#add-pledge').click(function(event) {
+		$('#new_pledge').submit();
+	});
+	$('#new_pledge').submit(function(e){
+		var slug=$('#slug').val();
+		$.ajax({
+			url: $(this).attr('action'),
+			type:'POST',
+			data : $(this).serialize(),
+			success : function (data){
+				$('#thank-modal-body').html('<h3>Cảm ơn bạn đã quyên góp cho chúng tôi với số tiền :'+data.pledge.pledge_amount+' đồng</h3>');
+				$('#back-to-project').attr('href', window.location.host);
+			}
+		});
+		e.preventDefault();
+	});
 
 })(jQuery);
