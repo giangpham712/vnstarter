1c8f0612da451d3ec4585210f0b169f6eca677aa
Wadey <wade712vn@gmail.com>
2015-03-27 12:39:19 +0100

    Commit

--- app/assets/javascripts/discover.js
+++ app/assets/javascripts/discover.js
@@ -1,5 +1,12 @@
-// Place all the behaviors and hooks related to the matching controller here.
-// All this logic will automatically be available in application.js.
-jQuery(document).ready(function($) {
-	$('.slides').slick();
-});
+(function($) {
+
+    $(function() {
+
+        $(".slider").slick({
+            vertical: true,
+            arrows: false
+        });
+
+    });
+
+})(jQuery)
--- app/controllers/discover_controller.rb
+++ app/controllers/discover_controller.rb
@@ -1,5 +1,6 @@
 class DiscoverController < ApplicationController
 	def index
+    @categories = Category.order(:name).all
     @projects = Project.where(deleted_at: nil, stopped_at: nil).where().not(launched_at: nil)
   end
 
