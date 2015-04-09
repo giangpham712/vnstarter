3b4c347e4a9297aa80d05bb143de44ed1de4cc8a
Wadey <wade712vn@gmail.com>
2015-04-09 13:16:17 +0200

    Commit

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,2 +1,11 @@
 module ProjectsHelper
+
+  def cache_key_for_project(project)
+    "/products/#{project.id}-#{project.updated_at}"
+  end
+
+  def cache_key_for_project_rewards(project)
+    "/products/#{project.id}/rewards"
+  end
+
 end
