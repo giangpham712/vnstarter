dead257c0a2a9221b785264d8e020c3d539445b5
Wadey <wade712vn@gmail.com>
2015-04-28 20:32:32 +0200

    Commit

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,15 +1,15 @@
 module ProjectsHelper
 
   def cache_key_for_project(project)
-    "products/#{project.id}-#{project.updated_at}"
+    "projects/#{project.id}-#{project.updated_at}"
   end
 
   def cache_key_for_project_posts(project)
-    "products/#{project.id}/posts/all"
+    "projects/#{project.id}/posts/all"
   end
 
   def cache_key_for_project_rewards(project)
-    "products/#{project.id}/rewards/all"
+    "projects/#{project.id}/rewards/all"
   end
 
 end
