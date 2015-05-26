75e4e2b56fc31b6bf0425afdc983de70afdceb9d
Hoang Giang <wade712vn@gmail.com>
2015-05-27 00:13:00 +0700

    Changes to landing page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
 	def index
+    @categories = Category.order(:name).all
 		@projects = Project.where(deleted_at: nil, stopped_at: nil)
                     .where().not(launched_at: nil)
                     .where("deadline > :now", { now: Time.zone.now }).take(3)
